4729ea2ed16ecff1923c51ce44caa16f522f19ab
shirenjiuhao <shirenjiuhao@qq.com>
2017-03-13 23:04:48 +0800

    updata 2017-03-13 add success

--- src/pages/nav1/Course.vue
+++ src/pages/nav1/Course.vue
@@ -43,32 +43,24 @@
 
 		<!--编辑界面-->
 		<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
-			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-				<el-form-item label="分类" prop='classifyName'>
-					<el-select v-model="editForm.classifyName" placeholder="请选择">
-					    <el-option
+			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm"  name='editForm' action="/momingtang/web/backCourse/updateCourse" enctype='multipart/form-data' id='editForm' method='post'
+			>
+				<el-form-item label="分类">
+					<select name="classifyId" class='classifyId' placeholder="请选择">
+					    <option
 					      v-for="item in options"
-					      :label="item.classifyName"
-					      :value="item.classifyName">
-					    </el-option>
-					  </el-select>
+					      :value="item.classifyId">{{item.classifyName}}
+					    </option>
+					  </select>
 				</el-form-item>
-				<el-form-item label="价格" prop='cPrice'>
-					<el-input v-model="editForm.cPrice" placeholder="请输入价格" class='myInput'></el-input>
+				<el-form-item label="价格">
+					<el-input name="cPrice" placeholder="请输入价格" class='myInput'></el-input>
 				</el-form-item>
-				<el-form-item label="名称" prop='courseName'>
-					<el-input v-model="editForm.courseName" placeholder="请输入内容" class='myInput'></el-input>
+				<el-form-item label="名称">
+					<el-input name="courseName" placeholder="请输入内容" class='myInput'></el-input>
 				</el-form-item>
 				<el-form-item label='背景图片'>
-					<el-upload
-					  class="upload-demo"
-					  ref="upload"
-					  :auto-upload="false"
-					  action="/momingtang/web/backCourse/updateCourse"
-					 >
-					<el-button slot="trigger" size="small" type="primary">选取图片</el-button>
-					<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-					</el-upload>
+					<input type="file" name='cover'>
 				</el-form-item>
 				
 				<!-- <el-form-item label="课程内容">
@@ -85,16 +77,15 @@
 
 		<!--新增界面-->
 		<el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
-			 <el-form :model='addForm' label-width="80px" ref="addForm" name='addForm' action="/momingtang/web/backCourse/addCourse" enctype='multipart/form-data' id='addForm'
+			 <el-form :model='addForm' label-width="80px" ref="addForm" name='addForm' action="/momingtang/web/backCourse/addCourse" enctype='multipart/form-data' id='addForm' method='post'
 			>
 				<el-form-item label="分类">
-					<el-select v-model='addForm.classifyId' name='classifyId' id='classifyId' placeholder="请选择">
-					    <el-option
+					<select name='classifyId' class='classifyId' placeholder="请选择">
+					    <option
 					      v-for="item in options"
-					      :label="item.classifyName"
-					      :value="item.classifyId">
-					    </el-option>
-					  </el-select>
+					      :value="item.classifyId">{{item.classifyName}}
+					    </option>
+					  </select>
 				</el-form-item>
 				<el-form-item label="名称">
 					<el-input name='courseName' placeholder="请输入内容" class='myInput'></el-input>
@@ -104,59 +95,19 @@
 				</el-form-item>
 				<el-form-item label='背景图片'>
 					<input type='file' name='cover'/>
-					<!-- <el-upload
-					  class="upload-demo"
-					  ref="upload1"
-					  list-type='file'
-					  :auto-upload="false"
-					  action="/momingtang/web/backCourse/addCourse"
-					 >
-					 <el-button slot="trigger" size="small" type="primary">选取图片</el-button>
-					</el-upload> -->
 				</el-form-item>
 				<el-form-item label="课程介绍">
 					<input type="file" name='detailsPic'>
-					<!-- <el-upload
-					  class="upload-demo"
-					  ref="upload2"
-					  :auto-upload="false"
-					  list-type='file'
-					  action="/momingtang/web/backCourse/addCourse"
-					 >
-					 <el-button slot="trigger" size="small" type="primary">选取图片</el-button> 
-					</el-upload>-->
 				</el-form-item>
 				<!-- <el-form-item label="课程内容">
 					<el-input v-model="addForm.msg" placeholder="第一天课题"></el-input>
 					<el-input v-model="addForm.msg1" placeholder="第二天课题"></el-input>
 					<el-input v-model="addForm.msg2" placeholder="第三天课题"></el-input>
 				</el-form-item> -->
-				
 			</el-form> 
-			<!-- <form action="/momingtang/web/backCourse/addCourse" method="post" enctype='multipart/form-data' id='addForm'>
-				<div>
-					 <span>类型：</span><input type="text" name="classifyId"/>
-				</div>
-				<div>
-					<span>名称：</span><input type="text" name="courseName"/>
-				</div>
-				<div>
-					<span>价格： </span><input type="number" name="cPrice"/>
-				</div>    
-				<div>
-					 <span>背景图片: </span><input type="file" name="cover"/>
-				</div>    
-				<div>
-					 <span>课程介绍：</span><input type="file" name="detailsPic"/>
-				</div>   
-				<div>
-					 <el-button @click.native="addFormVisible = false">取消</el-button>
-					<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
-				</div>   
-				</form>  -->
 			<div slot="footer" class="dialog-footer">
 				<el-button @click.native="addFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="addSubmit" id='addSubmit' :loading="addLoading">提交</el-button>
+				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
 			</div>
 				
 		</el-dialog>
@@ -166,7 +117,6 @@
 <script>
 	import util from '../../common/js/util'
 	import NProgress from 'nprogress'
-	//import { getUserListPage, removeUser, batchRemoveUser, editUser, addUser } from '../../api/api';
 	import $ from 'jquery'
 	import '../../jquery-from.js'
 	export default {
@@ -185,7 +135,7 @@
 				editFormVisible: false,//编辑界面是否显示
 				editLoading: false,
 				editFormRules: {
-					classifyName: [
+					/*classifyName: [
 						{ required: true, message: '请选择课程类型', trigger: 'blur' }
 					],
 					cPrice:[
@@ -193,7 +143,7 @@
 					],
 					courseName:[
 						{ required: true, message: '请输入课程名称', trigger: 'blur' }
-					]
+					]*/
 				},
 				//编辑界面数据
 				editForm: {
@@ -317,126 +267,27 @@
 			//显示新增界面
 			handleAdd: function () {
 				this.addFormVisible = true;
-				this.addForm = {
-					courseName: '',//名称
-					classifyName: '',
-					cPrice:'',
-					classifyId:'',//课程分类ID
-				};
 			},
 			//编辑
-			editSubmit: function () {
-				var _this = this ;
+			editSubmit: function (e) {
+				e.preventDefault();
 				this.$confirm('确认提交吗？', '提示', {}).then(() => {
 					this.editLoading = true;
 					NProgress.start();
-					this.addForm.cover = Object.assign({}, this.$refs.upload);
-					let para = Object.assign({}, this.editForm);
-					console.log(para)
-					$.ajax({
-						url: '/momingtang/web/backCourse/updateCourse',
-						type: 'POST',
-						data: para,
-					})
-					.then(function(res) {
-						console.log("success");
-						console.log(res)
-						this.editLoading = false;
-						NProgress.done();
-						if(res.status == 1){
-							this.$notify({
-								title: '成功',
-								message: '提交成功',
-								type: 'success'
-							});
-							this.$refs['editForm'].resetFields();
-						}else{
-							this.$notify({
-								title: '失败',
-								message: '提交失败',
-								type: 'err'
-							});
-						}
-						this.editFormVisible = false;
-						this.getUsers();
-					})
-					.fail(function(err) {
-						console.log("error");
-						console.log(err)
-					})
-					.always(function() {
-						console.log("complete");
-					});
-				});
-			},
-			//新增
-			addSubmit: function (e) {
-				e.preventDefault();
-				var _this = this
-				/*this.$confirm('确认提交吗？', '提示', {}).then(() => {
-					$('#fm').form('submit', {
-							   url : '${pageContext.request.contextPath}/web/active/addActivity',
-							   success : function(data) {
-							    if (data == 'ok') {
-							     $.messager.alert('消息', '保存成功！');
-							     $('#dg').datagrid('reload');
-							     $('#dlg').dialog('close');//关闭弹框                      
-							    }
-							   }
-							  });*/
-				/*this.$refs.addForm.validate((valid) => {
-					if (valid) {*/
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							console.log('--------------------------------------------')
-							this.addLoading = true;
-							NProgress.start();
-							alert(1)
-							let para = $('#addForm').serialize();
-							console.log(para);
-							
-								$('#addForm').ajaxSubmit({
-									url:'/momingtang/web/backCourse/addCourse',
-									tytpe:'post',
-									beforeSerialize:function(res){
-										let a = $('#classifyId').value
-										console.log(res)
-										console.log($('#classifyId').name);
-										/*switch(a){
-											case '基础课程':
-											$('#classifyId').value=1
-											break;
-											case '进阶课程':
-											$('#classifyId').value=2
-											break;
-											case '精进课程':
-											$('#classifyId').value=3
-
-										}*/
-									},
-									beforeSubmit:function(res){
-										console.log(res)
-									},
-									success: function(res){
-										console.log(res)
-										this.addLoading = false;
-										NProgress.done();
-										alert('消息保存成功！');
-									}
-								})
-					
-							
-							/*$.ajax({
-								cache: true,
-								url: '/momingtang/web/backCourse/addCourse',
-								type: 'POST',
-								async: false,
-								data: para
-							})
-							.done(function(res) {
-								console.log("success");
-								this.addLoading = false;
-								NProgress.done();
-								if(res.status == 1){
+					$('#editForm').ajaxSubmit({
+						url:'/momingtang/web/backCourse/updateCourse',
+						type:'post',
+						beforeSerialize:function(res){
+							console.log(res)
+						},
+						beforeSubmit:function(res){
+							console.log(res)
+						},
+						success: function(res){
+							console.log(res)
+							this.editLoading = false;
+							NProgress.done();
+							if(res.status == 1){
 									this.$notify({
 										title: '成功',
 										message: '提交成功',
@@ -452,30 +303,68 @@
 								}
 								this.addFormVisible = false;
 								this.getUsers();
-							}.bind(this))
-							.fail(function(err) {
-								console.log("error");
-								console.log(err)
-							})
-							.always(function() {
-								console.log("complete");
-							});*/
-							
-							/*addUser(para).then((res) => {
-								this.addLoading = false;
-								NProgress.done();
-								this.$notify({
-									title: '成功',
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['addForm'].resetFields();
+						}.bind(this),
+						error: function(res) {
+							this.editLoading = false;
+							NProgress.done();
+							this.$notify({
+								title: '失败',
+								message: '提交失败',
+								type: 'error'
+							});
+						}.bind(this)
+					})
+				});
+			},
+			//新增
+			addSubmit: function (e) {
+				e.preventDefault();
+				this.$confirm('确认提交吗？', '提示', {}).then(() => {
+					this.addLoading = true;
+					NProgress.start();
+					/*let para = $('#addForm').serialize();
+					  console.log(para);*/
+					$('#addForm').ajaxSubmit({
+						url:'/momingtang/web/backCourse/addCourse',
+						type:'post',
+						beforeSerialize:function(res){
+							console.log(res)
+						},
+						beforeSubmit:function(res){
+							console.log(res)
+						},
+						success: function(res){
+							console.log(res)
+							this.addLoading = false;
+							NProgress.done();
+							if(res.status == 1){
+									this.$notify({
+										title: '成功',
+										message: '提交成功',
+										type: 'success'
+									});
+									this.$refs['addForm'].resetFields();
+								}else{
+									this.$notify({
+										title: '失败',
+										message: '提交失败',
+										type: 'error'
+									});
+								}
 								this.addFormVisible = false;
 								this.getUsers();
-							});*/
-						});
-				/*	}
-				})*/
+						}.bind(this),
+						error: function(res) {
+							this.addLoading = false;
+							NProgress.done();
+							this.$notify({
+								title: '失败',
+								message: '提交失败',
+								type: 'error'
+							});
+						}.bind(this)
+					})
+				})
 			},
 			selsChange: function (sels) {
 				this.sels = sels;
@@ -535,4 +424,13 @@
 
 <style scoped>
 .myInput{width:218px;}
+.classifyId{
+	width: 218px;
+    height: 36px;
+    border: 1px solid #bfcbd9;
+    border-radius: 4px;
+    color: #959ac1;
+    padding: 3px 10px;
+    font-size: 14px;
+}
 </style>
